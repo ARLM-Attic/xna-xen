1cc5cb54c9143aed9d83e06f6b7a95eb0e650322
SND\StatusUnknown_cp <SND\StatusUnknown_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2008-09-28 21:33:37 +0000

    Project size reduced by compiling shaders as part of the prebuild process
    git-svn-id: https://xen.svn.codeplex.com/svn@2647 9b283d60-5439-405e-af05-b73fd8c4d996

--- prebuild.bat
+++ prebuild.bat
@@ -1,3 +1,26 @@
 @echo off
+@echo Building Xen.Ex.Filters FX
+xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Xen.Ex\Filters\Shader.fx" "Xen.Ex.Filters" "xen\src\Xen.Ex\Filters\Shader.fx.cs"
+@echo Building Xen.Ex.Graphics2D FX
+xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Xen.Ex\Graphics2D\FillTex.fx" "Xen.Ex.Graphics2D" "xen\src\Xen.Ex\Graphics2D\FillTex.fx.cs"
+xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Xen.Ex\Graphics2D\Shader.fx" "Xen.Ex.Graphics2D" "xen\src\Xen.Ex\Graphics2D\Shader.fx.cs"
+@echo Building Xen.Ex.Shaders FX
+xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Xen.Ex\Shaders\Simple.fx" "Xen.Ex.Shaders" "xen\src\Xen.Ex\Shaders\Simple.fx.cs"
+@echo Building Xen.Ex.Material FX
+xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Xen.Ex\Material\Material.fx" "Xen.Ex.Material" "xen\src\Xen.Ex\Material\Material.fx.cs"
+@echo Building Tutorials.Tutorial_03 FX
+xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Tutorials\Tutorial 03\shader.fx" "Tutorials.Tutorial_03" "xen\src\Tutorials\Tutorial 03\shader.fx.cs"
+@echo Building Xen and Xen.Ex DEBUG
 %SYSTEMROOT%\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe .\xen\prebuild\sln\prebuild.sln
+@echo -----------------------------------
+@echo -----------------------------------
+@echo - If part of the prebuild failed:
+@echo -
+@echo - Check the following are installed:
+@echo -
+@echo - .NET Framework 2.0
+@echo - XNA Game Studio 2.0
+@echo - DirectX SDK
+@echo -----------------------------------
+@echo -----------------------------------
 pause
