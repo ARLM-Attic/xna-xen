c36121a2a37f823985df467783d9ef896c9f37bc
SND\StatusUnknown_cp <SND\StatusUnknown_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2008-10-31 11:45:21 +0000

    Version 1.2 (XNA 3.0)
    Remember to run prebuild when updating!
    
    This update supports XNA 3.0 only.
    Support for XNA 2.0 and XNA 3.0 Beta have been removed.
    
    [Fixed] The model importer now correctly handles models without animations / materials
    
    git-svn-id: https://xen.svn.codeplex.com/svn@7089 9b283d60-5439-405e-af05-b73fd8c4d996

--- xen prebuild.bat
+++ xen prebuild.bat
@@ -1,17 +1,29 @@
 @echo off
-@echo Prebuilding xen (XNA GS 2.0)
+@echo.
+@echo -----------------------------------------------------
+@echo -----------------------------------------------------
+@echo - 
+@echo - This process will prebuild xen (Debug) for XNA 3.0.
+@echo - (Note XNA 2.0 and 3.0 Beta are no longer supported)
+@echo - 
+@echo - A VisualStudio CustomTool plugin will be installed,
+@echo - which may prompt for admin rights in Windows Vista.
+@echo - 
+@echo - Please report any bugs you find - and of course
+@echo - I greatly appreciate all feedback, good or bad!
+@echo - 
+@echo - Thank you :-)
+@echo -
+@echo -----------------------------------------------------
+@echo -----------------------------------------------------
+@echo.
+pause
+@echo.
+@echo Prebuilding xen for XNA GS 3.0 (This may take a minute or so...)
+@echo.
 @echo Register Visual Studio Custom Tool
 "xen\bin\Xen.Graphics.ShaderSystem.CustomTool\registry\XenFxRegistryTest.exe" "xen\bin\Xen.Graphics.ShaderSystem.CustomTool\registry\XenFxRegistrySetup.exe" "xen\bin\Xen.Graphics.ShaderSystem.CustomTool\Xen.Graphics.ShaderSystem.CustomTool.dll" "XenFX" "{43ACA195-467F-4EEC-A949-5873BBD5413A}"
-@echo Copying Xen Shader System files
-copy "xen\prebuild\bin\XNAGS_2_0\x86\Xen.Graphics.ShaderSystem.dll" "xen\bin\x86\" /Y /V
-copy "xen\prebuild\bin\XNAGS_2_0\x86\Xen.Graphics.ShaderSystem.xml" "xen\bin\x86\" /Y /V
-copy "xen\prebuild\bin\XNAGS_2_0\Xbox 360\Xen.Graphics.ShaderSystem.dll" "xen\bin\Xbox 360\" /Y /V
-copy "xen\prebuild\bin\XNAGS_2_0\Xbox 360\Xen.Graphics.ShaderSystem.xml" "xen\bin\Xbox 360\" /Y /V
-@echo Copying Xen Content Project files
-copy "xen\src\Xen\ContentProject\XNAGS_2_0\Content.contentproj" "xen\src\Xen\Content\" /Y /V
-copy "xen\src\Xen.Ex\ContentProject\XNAGS_2_0\Content.contentproj" "xen\src\Xen.Ex\Content\" /Y /V
-copy "xen\src\Tutorials\ContentProject\XNAGS_2_0\Content.contentproj" "xen\src\Tutorials\Content\" /Y /V
-@echo Building Xen.Ex Shaders (This may take a minute or so...)
+@echo Building Xen.Ex Shaders
 @echo Building Xen.Ex.Filters FX
 xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Xen.Ex\Filters\Shader.fx" "Xen.Ex.Filters" "xen\src\Xen.Ex\Filters\Shader.fx.cs"
 @echo Building Xen.Ex.Graphics2D FX
@@ -30,16 +42,23 @@ xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Tutorials\Tut
 @echo Building Tutorials.Tutorial_16 FX
 xen\bin\Xen.Graphics.ShaderSystem.CustomTool\cmdxenfx.exe "xen\src\Tutorials\Tutorial 16\shader.fx" "Tutorials.Tutorial_16" "xen\src\Tutorials\Tutorial 16\shader.fx.cs"
 @echo Building Xen and Xen.Ex DEBUG
-%SYSTEMROOT%\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe /t:Rebuild .\xen\prebuild\sln\prebuild.sln
+%SYSTEMROOT%\Microsoft.NET\Framework\v3.5\MSBuild.exe /t:Rebuild .\xen\prebuild\sln\prebuild.sln
+@echo.
 @echo -----------------------------------
 @echo -----------------------------------
 @echo - If part of the prebuild failed:
 @echo -
 @echo - Check the following are installed:
 @echo -
-@echo - .NET Framework 2.0
-@echo - XNA Game Studio 2.0
+@echo - .NET Framework 3.5
+@echo - XNA Game Studio 3.0
 @echo - DirectX SDK
 @echo -----------------------------------
 @echo -----------------------------------
+@echo.
+@echo -----------------------------------
+@echo - To get started, open:
+@echo - ./xen/Tutorials.sln
+@echo -----------------------------------
+@echo.
 pause
